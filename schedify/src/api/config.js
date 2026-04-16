7cb31b98e23297f6a119bb4ded629e95b9ebbca8
ADARSH DUBEY <139523805+adarshiiit0117@users.noreply.github.com>
2026-04-16 16:26:22 +0530

    Update config.js

--- schedify/src/api/config.js
+++ schedify/src/api/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'https://finalassignment-2.onrender.com';
 
 export const api = axios.create({
   baseURL: BASE_URL,
